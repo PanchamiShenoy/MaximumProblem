b7b50fbb97c14023b093540190fad957ed4aa05a
PanchamiShenoy <panchamishenoy16@gmail.com>
2021-09-08 18:45:01 +0530

    generic class is created

--- src/com/max/MaximumTest.java
+++ src/com/max/MaximumTest.java
@@ -1,11 +1,22 @@
 package com.max;
 
-public class MaximumTest {
+public class MaximumTest<T extends Comparable<T>> {
+	public T x, y, z;
+
+	public MaximumTest(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public void findMax() {
+		T max = MaximumTest.findMaximum(x, y, z);
+	}
 
 	/*
-	 * generic method to find max 
+	 * generic method to find max
 	 */
-	public <T extends Comparable<T>> T findMaximum(T x, T y, T z) {
+	public static <T extends Comparable<T>> T findMaximum(T x, T y, T z) {
 		T max = x;
 		if (y.compareTo(max) > 0) {
 			max = y;
@@ -13,19 +24,21 @@ public class MaximumTest {
 		if (z.compareTo(max) > 0) {
 			max = z;
 		}
-		System.out.printf("\nMax of" + x + " " + y + " " + z + "  is" + max);
+		System.out.println("\nMaximum of " + x + "," + y + " and " + z + " is " + max);
 		return max;
 
 	}
 
 	public static void main(String[] args) {
 		String s1 = "Apple", s2 = "Peach", s3 = "Banana";
-		MaximumTest t1 = new MaximumTest();
-		t1.findMaximum(s1, s2, s3);
-		MaximumTest t2 = new MaximumTest();
-		t2.findMaximum(s2, s3, s1);
-		MaximumTest t3 = new MaximumTest();
-		t3.findMaximum(s3, s1, s2);
+		MaximumTest t1 = new MaximumTest(s1, s2, s3);
+		t1.findMax();
+		Integer num1 = 100, num2 = 200, num3 = 300;
+		MaximumTest t2 = new MaximumTest(num1, num2, num3);
+		t2.findMax();
+		Float f1 = 999.9f, f2 = 999.8f, f3 = 999.79f;
+		MaximumTest t3 = new MaximumTest(f1, f2, f3);
+		t3.findMax();
 	}
 
 }
